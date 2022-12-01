b3a8aad004c0f15dc6ff3543461d42efa27235de
Lydia Taban <tabanlydia@gmail.com>
2022-12-01 14:53:53 +0000

    Print names defined by the compiled module hidden_4.pyc

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import hidden_4
     names = dir(hidden_4)
-    for name in names:
-        if "__" not in name:
-            print(name)
+    for a in names:
+        if a[:2] != "__":
+            print(a)
    
