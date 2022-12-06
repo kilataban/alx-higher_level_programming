2892bbdfb6dcf8dc9f23f0908aa611db7dc8bd21
Lydia Taban <tabanlydia@gmail.com>
2022-12-06 02:05:38 +0000

    Show Python lists using C

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,25 @@
+#include <Python.h>
+
+/**
+ * print_python_list_info - prints python lists information and descriptions
+ * @p: PyObject list to use
+ */
+void print_python_list_info(PyObject *p)
+{
+	int size, alloc, x;
+	PyObject *obj;
+
+	size = Py_SIZE(p);
+	alloc = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", alloc);
+
+	for (x = 0; x < size; x++)
+	{
+		printf("Element %d: ", x);
+
+		obj = PyList_GetItem(p, x);
+		printf("%s\n", Py_TYPE(obj)->tp_name);
+	}
+}
