90d6808f015a70d23b464113407f8f5a5bbb03a1
Lydia Taban <tabanlydia@gmail.com>
2022-12-06 01:53:27 +0000

    Technical interview prep question: Palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,24 @@
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - determines if the word is a palindrome
+ * @head: pointer to the head node
+ * Return: On Success (1), else (0)
+ */
+int is_palindrome(listint_t **head)
+{
+	unsigned int len = 1;
+	listint_t *temp;
+
+	if (head == NULL|| *head == NULL)
+		return (0);
+
+	temp = *head;
+	while (temp)
+	{
+		temp = temp->next;
+		len++;
+	}
+	return (1);
+}
