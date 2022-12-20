bb46c14ccbeb9c62a9a2c73887bd3bbed16b67cf
Lydia Taban <tabanlydia@gmail.com>
2022-12-20 23:42:17 +0000

    Access and update private size attribute using getter and setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# 4-square.py
+"""Define Square class and update the private size attr """
+
+
+class Square(object):
+    """define the class variables"""
+    def __init__(self, size=0):
+        """initiate size variable value"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """define size method/property"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """class variable size"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Define area method"""
+        return self.__size * self.
