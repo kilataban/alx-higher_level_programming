aa826a0bed046ba009e640a27d5dc947f91b7ab6
Lydia Taban <tabanlydia@gmail.com>
2023-01-10 21:16:44 +0000

    Intial commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+# 0-read_file.py
+"""define text file-reading function."""
+
+
 
 def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
+    """print file contents to std output"""
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
