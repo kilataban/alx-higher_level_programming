d4eb38868274fdbddc512c116b1a6f1bebdc9724
Lydia Taban <tabanlydia@gmail.com>
2023-01-10 22:04:39 +0000

    Return the dictionary description with simple data structure

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 8-class_to_json.py
+"""define function that returns dictionary description"""
+
+
+def class_to_json(obj):
+    """returns dictionary decription with sample data structure"""
+    return obj.__dict__
